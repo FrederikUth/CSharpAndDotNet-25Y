b5d0ed82c6019655af28180648ea28fccd6ddc28
FrederikUth <uthfrederik@gmail.com>
2026-08-19 09:59:23 +0200

    ses

---
+++ Lektion01/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student();
+        student.Name = "Frede";
+        student.Age = 24;
+        Console.WriteLine($"{student.Name} har alderen {student.Age}");
+    }
+}
---
+++ Lektion01/ConsoleApp1/Student.cs
@@ -0,0 +1,7 @@
+namespace ConsoleApp1;
+
+public class Student
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
