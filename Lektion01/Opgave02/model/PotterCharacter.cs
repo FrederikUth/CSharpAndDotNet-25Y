a72e11a8724036a3f0b22137c08bc74c1baacd67
FrederikUth <uthfrederik@gmail.com>
2026-09-09 11:46:46 +0200

    tilføjet lektion1

---
+++ Lektion01/Opgave02/model/PotterCharacter.cs
@@ -0,0 +1,4 @@
+namespace Opgave02.model;
+
+public record PotterCharacter(string fullName, string nickname, string hogwartsHouse, string interpretedBy, List<string> children, string image, string birthdate, int index);
+
---
+++ Lektion01/Opgave03/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Opgave03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount b1 = new BankAccount();
+    }
+}
